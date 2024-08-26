27cbc20925da22edb7fb8244e67b0fab14d49ea4
Ian Bui <thinkbui@gmail.com>
2024-08-25 21:44:32 -0500

    Completed Mercari function and updated link text format

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,14 @@ let formEbaySubmit = function(event){
   event.preventDefault();
   let listing_id_val = listing_id_form_input.value;
   let link_url = EBAY_LINK_BASE + listing_id_val;
-  genLink(link_url, EBAY_LINK_NAME);
+  genLink(link_url, EBAY_LINK_NAME, listing_id_val);
+}
+
+let formMercariSubmit = function(event){
+  event.preventDefault();
+  let listing_id_val = listing_id_form_input.value;
+  let link_url = MERCARI_LINK_BASE + listing_id_val;
+  genLink(link_url, MERCARI_LINK_NAME, listing_id_val);
 }
 
 let formReset = function(event){
@@ -25,12 +32,12 @@ let clearGenLink = function(){
   gen_link_elem.innerHTML = DEFAULT_GEN_LINK_CONTENT;
 }
 
-let genLink = function(url, name){
+let genLink = function(url, name, itm_num){
   let gen_lnk = document.createElement("a");
   gen_lnk.href = url;
   gen_lnk.target = "_blank";
   gen_lnk.rel = "noopener noreferrer";
-  gen_lnk.innerHTML = ["Go to",name ,"🔗"].join(" ");
+  gen_lnk.innerHTML = ["🔗 Go to",name,itm_num].join(" ");
   gen_link_elem.innerHTML = "";
   gen_link_elem.appendChild(gen_lnk);
 }
