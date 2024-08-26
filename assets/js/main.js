9958cc562108ddc70c4ab2c74f68853ddd7b8204
Ian Bui <thinkbui@gmail.com>
2024-08-25 22:54:05 -0500

    Removed old submit buttons and functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,4 @@
 const DEFAULT_GEN_LINK_CONTENT = "(pending generation)";
-
-const EBAY_LINK_NAME = "eBay";
-const EBAY_LINK_BASE = "https://www.ebay.com/itm/";
-const MERCARI_LINK_NAME = "Mercari";
-const MERCARI_LINK_BASE = "https://www.mercari.com/us/item/";
-const WALMART_LINK_NAME = "Walmart";
-const WALMART_LINK_BASE = "https://www.walmart.com/ip/";
-
 const LINK_DATA = {
                     "eBay": {
                               "name": "eBay",
@@ -30,27 +22,6 @@ let gen_link_elem = document.getElementById("generated_link");
 let listing_id_form_input = document.getElementById("form_input");
 let listing_id = "";
 
-// TODO: Combine these into single function as these are basically identical
-let formEbaySubmit = function(event){
-  event.preventDefault();
-  let listing_id_val = listing_id_form_input.value;
-  let link_url = EBAY_LINK_BASE + listing_id_val;
-  genLink(link_url, EBAY_LINK_NAME, listing_id_val);
-}
-
-let formMercariSubmit = function(event){
-  event.preventDefault();
-  let listing_id_val = listing_id_form_input.value;
-  let link_url = MERCARI_LINK_BASE + listing_id_val;
-  genLink(link_url, MERCARI_LINK_NAME, listing_id_val);
-}
-
-let formWalmartSubmit = function(event){
-  event.preventDefault();
-  let listing_id_val = listing_id_form_input.value;
-  let link_url = WALMART_LINK_BASE + listing_id_val;
-  genLink(link_url, WALMART_LINK_NAME, listing_id_val);
-}
 
 let formSubmit = function(event, key){
   event.preventDefault();
